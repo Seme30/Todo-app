03f6a933330991fa730b0d98c26250e18989bc3c
Semahegn <83661382+Seme30@users.noreply.github.com>
2022-07-12 14:27:51 +0300

    constants

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,8 @@
+
+class AppConstants {
+  static const String APP_NAME = "SEME TODOAPP";
+  static const int APP_VERSION = 1;
+  
+  static const String BASE_URL = "http://localhost:8080";
+  static const String TODO = 'api/v1/todoitems';
+}
