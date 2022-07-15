11e0bc562bdf7ff9ee774efd1fa4aa5be44726eb
Semahegn <83661382+Seme30@users.noreply.github.com>
2022-07-15 21:05:21 +0300

    constants

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -3,6 +3,7 @@ class AppConstants {
   static const String APP_NAME = "SEME TODOAPP";
   static const int APP_VERSION = 1;
   
-  static const String BASE_URL = "http://localhost:8080";
-  static const String TODO = 'api/v1/todoitems';
+  static const String BASE_URL = "https://todo-rest-api-seme30.herokuapp.com";
+  static const String TODO ='https://todo-rest-api-seme30.herokuapp.com/api/v1/todoitems';
+
 }
---
+++ lib/constants/colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color textColor =  Color(0xFFccc7c5);
+  static const Color mainColor =  Color.fromRGBO(1, 49, 88, 1);
+
+  static const Color iconColor1 = Color(0xffffd28d);
+  static const Color iconColor2 = Color(0xfffcab88);
+  static const Color paraColor = Color(0xff8f837f);
+  static const Color buttonBackgroundColor = Color(0xfff7f6f4);
+  static const Color signColor = Color(0xffa9a29f);
+  static const Color titleColor = Color(0xff5c524f);
+  static const Color mainBlackColor = Color(0xFF332dfb);
+
+  static const Color yellowColor = Color(0xffffd379);
+}
