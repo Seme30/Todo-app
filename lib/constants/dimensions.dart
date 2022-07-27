2b7c105c89b90fec566f4c5e49bf2fb819bb72ae
Semahegn <83661382+Seme30@users.noreply.github.com>
2022-07-27 22:36:40 +0300

    dimensions

--- lib/constants/dimensions.dart
+++ lib/constants/dimensions.dart
@@ -4,43 +4,43 @@ class Dimensions {
   static double screenHeight = Get.height;
   static double screenWidth = Get.width;
 
-  static double pageView = screenHeight/2.64;
-  static double pageViewContainer = screenHeight/3.84;
-  static double pageViewTextContainer = screenHeight/7.03;
+  static double pageView = screenHeight / 2.64;
+  static double pageViewContainer = screenHeight / 3.84;
+  static double pageViewTextContainer = screenHeight / 7.03;
 
   //dynamic height padding and margin
-  static double height10 = screenHeight/84.4;
-  static double height15 = screenHeight/56.27;
-  static double height20 = screenHeight/42.2;
-  static double height30 = screenHeight/28.13;
-  static double height45 = screenHeight/18.76;
+  static double height10 = screenHeight / 84.4;
+  static double height15 = screenHeight / 56.27;
+  static double height20 = screenHeight / 42.2;
+  static double height30 = screenHeight / 28.13;
+  static double height45 = screenHeight / 18.76;
+  static double height54 = screenHeight / 15.63;
+  static double height450 = screenHeight / 1.876;
 
   //dynamic width padding and margin
-  static double width10 = screenHeight/84.4;
-  static double width15 = screenHeight/56.27;
-  static double width20 = screenHeight/42.2;
-  static double width30 = screenHeight/28.13;
+  static double width10 = screenHeight / 84.4;
+  static double width15 = screenHeight / 56.27;
+  static double width20 = screenHeight / 42.2;
+  static double width30 = screenHeight / 28.13;
 
   //font Size
-  static double font16 = screenHeight/52.75;
-  static double font20 = screenHeight/42.2;
-  static double font26 = screenHeight/32.46;
+  static double font16 = screenHeight / 52.75;
+  static double font20 = screenHeight / 42.2;
+  static double font26 = screenHeight / 32.46;
 
   //radius
-  static double radius15 = screenHeight/56.27;
-  static double radius20 = screenHeight/42.2;
-  static double radius30 = screenHeight/28.13;
+  static double radius15 = screenHeight / 56.27;
+  static double radius20 = screenHeight / 42.2;
+  static double radius30 = screenHeight / 28.13;
 
   //icon size
-  static double iconSize24 = screenHeight/35.17;
-  static double iconSize16 = screenHeight/52.75;
+  static double iconSize24 = screenHeight / 35.17;
+  static double iconSize16 = screenHeight / 52.75;
 
   //list view size
-  static double listViewImgSize = screenWidth/3.25;
-  static double listViewtextContSize = screenWidth/3.9; 
+  static double listViewImgSize = screenWidth / 3.25;
+  static double listViewtextContSize = screenWidth / 3.9;
 
   //bottom height
-  static double bottomHeightBar = screenHeight/7.03;
-
-
+  static double bottomHeightBar = screenHeight / 7.03;
 }
